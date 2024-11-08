6c616975c97fc3327c22ac1678eff68f1cd1dadd
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2024-11-08 09:57:01 +0330

    add server information in config

--- system/config.php
+++ system/config.php
@@ -1,12 +1,15 @@
 <?php
 
-$base_url = "http://localhost/mvc/";
-$base_dir = "/mvc/";
+$base_url = "http://localhost/tiny_project/";
+$base_dir = "/tiny_project/";
 
 $tmp = explode('?', $_SERVER['REQUEST_URI']);
 $current_route = str_replace($base_dir,'',$tmp[0]);
 unset($tmp);
-
+$dbHost = 'localhost';
+$dbName = 'mvc_blog';
+$dbUsername = 'root';
+$dbPassword = '';
 
 
 
