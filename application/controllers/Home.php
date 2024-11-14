c40098073dbc6d233e68f8361ebae1172ea9cc07
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2024-11-14 19:59:01 +0330

    add contorlers

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,12 +5,13 @@ namespace Application\Controllers;
 class Home extends Controller{
 
     public function index(){
-        $productName = "phone";
-        $this->view('app.index',compact('productName'));
+  
     }
 
-    public function create(){
-        echo "this is a create method";
+    public function category($id){
+    }
+    
+    public function show($id){
     }
 
 
