9c3dcaa5bbb56b97abb58d9a5dbde5ef624e5fa6
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2024-11-17 20:35:28 +0330

    model

--- application/model/Article.php
+++ application/model/Article.php
@@ -1,26 +1,31 @@
 <?php
 
 namespace application\model;
-class Article extends Model {
-    public function all(){
-        $query= "SELECT * FROM `articles`";
+
+class Article extends Model
+{
+    public function all()
+    {
+        $query = "SELECT * FROM `articles`";
         $result = $this->query($query)->fetchAll();
         $this->closeConnection();
         return $result;
     }
-    public function find($id){
-$query = "SELECT * ,(SELECT `name` FROM `categories` WHERE `categories`.`id` = `articles`.`cat_id` ;) as category FROM `articles` WHERE id = ? ";
-$result=$this->query($query , [$id])->fetch();
-$this->closeConnection();
-return $result ;
+    public function find($id)
+    {
+        $query = "SELECT * ,(SELECT `name` FROM `categories` WHERE `categories`.`id` = `articles`.`cat_id` ;) as category FROM `articles` WHERE id = ? ";
+        $result = $this->query($query, [$id])->fetch();
+        $this->closeConnection();
+        return $result;
     }
-    public function insert($values){
-$query = "INSERT INTO"
+    public function insert($values)
+    {
+        $query = "INSERT INTO `articles` (`title` , `cat_id`,`body`, created_at) VALUES (?,?,?,now())";
+        $this->execute($query , array_values($values));
+        $this->closeConnection();
     }
-    public function update($id , $values){
-
+    public function update($id, $values) {
+        $query= "UPDATE `articles` SET `title` = ? , `cat_id` = ?, `body` =  ? , `updated_at` = now() ";
     }
-    public function delete($id){
-
-    }
+    public function delete($id) {}
 }
