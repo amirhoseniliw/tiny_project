3a8492c1149bf1227282d64d89facb998ceaf027
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2024-11-21 14:39:55 +0330

    contorols

--- application/controllers/Article.php
+++ application/controllers/Article.php
@@ -1,25 +1,39 @@
-<?php 
+<?php
+
 namespace Application\Controllers;
-class Article extends Controller{
-    public function index(){
 
-    }
-    public function create(){
+use application\model\Article as ArticleModel;
+use application\model\Category as CategoryModel;
 
+class Article extends Controller
+{
+    public function index()
+    {
+        $article = new ArticleModel();
+        $articles = $article->all();
+        return $this->View('panel.article.index', compact('articles'));
     }
-    public function store(){
-
+    public function create()
+    {
+        $category = new CategoryModel();
+        $categories = $category->all();
+        return $this->View('panel.article.create', compact('categories'));
     }
-    public function show($id){
-
+    public function store()
+    {
+        // $article = new ArticleModel();
+        // $articles = $article->insert();
     }
-    public function edit($id){
-
+    public function show($id) {
+        $article = new ArticleModel();
+        $articles = $article->find($id);
+        return $this->View('panel.article.show', compact('articles'));
     }
-    public function update($id){
-
+    public function edit($id) {
+        $article = new ArticleModel();
+        $articles = $article->find($id);
+        return $this->View('panel.article.edit', compact('articles'));
     }
-    public function destroy($id){
-
-    }
+    public function update($id) {}
+    public function destroy($id) {}
 }
