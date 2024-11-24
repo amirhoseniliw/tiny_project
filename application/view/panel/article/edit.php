f41238c07f40d0f24c438273cbf1399b854aa7be
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2024-11-24 17:36:11 +0330

    view

--- application/view/panel/article/edit.php
+++ application/view/panel/article/edit.php
@@ -1,19 +1,22 @@
 <?php $this->include('panel.layouts.header'); ?>
-<form action="<?php $this->url('article/update'); ?>" method="POST">
+<form action="<?php $this->url('article/update'. $article['id']); ?>" method="POST">
     <section class="form-group">
         <label for="title">Title</label>
-        <input type="text" class="form-control" id="title" placeholder="title ..." value="<?= $article['title']?>">
+        <input type="text" class="form-control" name="title" id="title" placeholder="title ..." value="<?= $article['title']?>">
     </section>
     <section class="form-group">
         <label for="cat_id">Category</label>
-        <select class="form-control" id="cat_id">
-            <option value="1">Sport</option>
-            <option value="2">News</option>
+        <select class="form-control" id="cat_id" name="cat_id">
+            <?php foreach ($categories as $category) {
+             ?>
+            <option value="<?= $category['name']?> <?php if ($category['id'] == $article['cat_id']) {echo "selected";
+            } ?>"><?= $category['name']?></option>
+           <?php }?>
         </select>
     </section>
     <section class="form-group">
         <label for="body">Body</label>
-        <textarea class="form-control" id="body" rows="5" placeholder="body ..."></textarea>
+        <textarea class="form-control" id="body"  name="body" rows="5" placeholder="body ..." ><?= $article['body'] ?></textarea>
     </section>
     <button type="submit" class="btn btn-primary">Update</button>
     </form>
