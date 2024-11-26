9516ae75e42df3a6e0daf47d07f4d61320412dab
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2024-11-26 19:14:39 +0330

    view

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,14 +1,24 @@
 <?php
 
 namespace Application\Controllers;
-
+use application\model\Article as ArticleModel;
+use application\model\Category as CategoryModel;
 class Home extends Controller{
 
     public function index(){
-  
+        $category = new CategoryModel();
+        $categories = $category->all();
+        $article = new ArticleModel();
+        $articles = $article->all();
+        return $this->View('app.index', compact('categories', 'articles'));
+
     }
 
     public function category($id){
+        $ob_category = new CategoryModel();
+        $categories = $ob_category->all();
+        $ob_category = new CategoryModel();
+         $article = $ob_category->article($id);
     }
     
     public function show($id){
