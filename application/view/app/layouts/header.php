6be868ca2d069e3cc7765b0aa213bf9654e68a8d
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2024-11-30 19:19:13 +0330

    view

--- application/view/app/layouts/header.php
+++ application/view/app/layouts/header.php
@@ -19,14 +19,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="<?php $this->url('/home' )?>">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <?php
-        var_dump($categories);
                  foreach ($categories as $category) {
             ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="category.html"><?= $category['name'] ?></a>
+                    <a class="nav-link" href="<?php $this->url('/home/category/' . $category['id']) ?>"><?= $category['name'] ?></a>
                 </li><?php } ?>
                 
             </ul>
