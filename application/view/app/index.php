dba7a24eb5873e0c7ccd304bb8de9c2361f84a04
amirhoseniliw <amirhoseniliw.951855@gmail.com>
2024-11-30 17:04:30 +0330

    view

--- application/view/app/index.php
+++ application/view/app/index.php
@@ -3,12 +3,13 @@
     <section class="container my-5">
         <!-- Example row of columns -->
         <section class="row">
-            <?php foreach ($articles as $article) {
+            <?php 
+             foreach ($articles as $article) {
              ?>
             <section class="col-md-4">
-                <h2>Heading</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                <p><a class="btn btn-primary" href="detail.html" role="button">View details »</a></p>
+                <h2><?= $article['title'] ?></h2>
+                <p><?= $article['body'] ?></p>
+                <p><a class="btn btn-primary" href="<?php $this->url('/home/show/' . $article['id']) ?>" role="button">View details »</a></p>
             </section></br></br>
            <?php }?>
         </section>
